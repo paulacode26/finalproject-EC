de17b196c7597c378594e0598c27ff4b0b83e7ad
sandra-vera1 <sandra-vera1@hotmail.com>
2025-03-27 15:04:17 -0600

    Tested

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>group 1</h1>
+      <h1>Group 1</h1>
       <h2>Paula Gomez Perez</h2>
       <h2>Sandra Vera Gomez</h2>
       <h2>Nafiyad Adane Gudina</h2>
