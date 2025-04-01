3354d3aab74aca740c6a0d5c0f266f03097282b3
sandra-vera1 <sandra-vera1@hotmail.com>
2025-04-01 09:55:30 -0600

    Modified files App.js and App test.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Group 1</h1>
+      <h1>Group 1 - Enterprise Computing</h1>
       <h2>Paula Gomez Perez</h2>
       <h2>Sandra Vera Gomez</h2>
-      <h2>Nafiyad Adane Gudina</h2>
       <h2>Erick Mulia Goycoolea</h2>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const titleElement = screen.getByText(/group 1/i);
+  const titleElement = screen.getByText(/Group 1 - Enterprise Computing/i);
   expect(titleElement).toBeInTheDocument();
 
   const member1 = screen.getByText(/paula gomez perez/i);
@@ -11,9 +11,6 @@ test('renders learn react link', () => {
 
   const member2 = screen.getByText(/sandra vera gomez/i);
   expect(member2).toBeInTheDocument();
-
-  const member3 = screen.getByText(/nafiyad adane gudina/i);
-  expect(member3).toBeInTheDocument();
 
   const member4 = screen.getByText(/erick mulia goycoolea/i);
   expect(member4).toBeInTheDocument();
